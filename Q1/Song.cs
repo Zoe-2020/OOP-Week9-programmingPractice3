f22bb9441878779dd4a0b767852c1fa329852d98
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2020-11-26 18:03:08 +0000

    Q6

--- Q1/Song.cs
+++ Q1/Song.cs
@@ -6,11 +6,27 @@ namespace Q1
 {
     class Song
     {
-        public string title { get; set; }
-        public string artist { get; set; }
-        public double duration { get; set; }
-
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
         public Genre MusicGenre { get; set; }
+
+       public Song(string title,string artist,double duration, Genre musicGene)
+        {
+            title = Title;
+            artist = Artist;
+            duration = Duration ;
+            musicGene = MusicGenre;
+        }
+
+        public Song(string title, string artist) : this(title,artist, 0, Genre.Other) { }
+
+        public Song() : this("unknown", "unknown") { }
+
+        public override string ToString()
+        {
+            return string.Format($"{Title}{Artist}{Duration}{MusicGenre}");
+        }
 
     }
     public enum Genre { Rock,Pop,Dance,Other }
