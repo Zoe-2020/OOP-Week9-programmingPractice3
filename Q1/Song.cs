13cf9873cc20271f35620252c8ce3dec64aa5e40
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2020-11-26 17:10:19 +0000

    Q4

---
+++ Q1/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q1
+{
+    class Song
+    {
+        public string title { get; set; }
+        public string artist { get; set; }
+        public double duration { get; set; }
+
+        public Genre MusicGenre { get; set; }
+
+    }
+    public enum Genre { Rock,Pop,Dance,Other }
+}
